5166c80c5e98f057bab7ce37006013db1fa6173b
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-04-08 12:55:29 +0200

    Added Launcher and UI for RoomListing

---
+++ BussenOnline/Assets/Scripts/Launcher.cs
@@ -0,0 +1,119 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Public Vars
+    [Tooltip("The loglevel of Photon")]
+    public PunLogLevel Loglevel = PunLogLevel.Informational;
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte MaxPlayersPerRoom = 4;
+    [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+    [SerializeField]
+    private GameObject controlPanel;
+    [Tooltip("The UI Label to inform the user that the connection is in progress")]
+    [SerializeField]
+    private GameObject progressLabel;
+    #endregion
+
+    #region Private Vars
+    string gameVersion = "1";
+    bool isConnecting;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true; //Make sure the same scene is loaded
+        PhotonNetwork.LogLevel = Loglevel;
+    }
+
+    void Start()
+    {
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+    }
+
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Public Methods
+    /// <summary>
+    /// Start the connection process.
+    /// - If already connected, we attempt joining a random room
+    /// - if not yet connected, Connect this application instance to Photon Cloud Network
+    /// </summary>
+    public void Connect()
+    {
+        Debug.Log("The start game button has been pressed");
+
+        progressLabel.SetActive(true);
+        controlPanel.SetActive(false);
+
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            isConnecting = PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+    #endregion
+
+    #region Photon.PunBehaviour CallBacks
+    /// <summary>
+    /// Gets called upon connecting to the master server
+    /// </summary>
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Launcher: OnConnectedToMaster() was called by PUN");
+
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+            isConnecting = false;
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarning("Launcher: OnDisconnected() was called by PUN with error: " + cause);
+
+        progressLabel.SetActive(false);
+        controlPanel.SetActive(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        string roomName = null;
+        RoomOptions roomOptions = new RoomOptions() { MaxPlayers = MaxPlayersPerRoom };
+        TypedLobby typedLobby = null;
+
+        PhotonNetwork.CreateRoom(roomName, roomOptions, typedLobby);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Launcher: OnJoinedRoom() called by PUN. Now this client is in a room with nickname: " + PhotonNetwork.NickName);
+
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("We load the GameScene");
+
+            PhotonNetwork.LoadLevel("GameScene");
+        }
+    }
+    #endregion
+}
---
+++ BussenOnline/Assets/Scripts/NickNameInputField.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+[RequireComponent(typeof(InputField))]
+public class NickNameInputField : MonoBehaviour
+{
+    #region Const Variables
+    const string playerNamePrefKey = "PlayerName"; //The name of the key for playername
+    #endregion
+
+    #region MonoBehaviour CallBacks
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField inputField = GetComponent<InputField>();
+
+        if (inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.NickName = defaultName;
+    }
+    #endregion
+
+    #region Public Methods
+    /// <summary>
+    /// Sets the name of the player, and save it in the PlayerPrefs for future sessions.
+    /// </summary>
+    /// <param name="value">The name of the player</param>
+    public void SetPlayerName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Name is null or empty");
+            return;
+        }
+
+        PhotonNetwork.NickName = value;
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+    #endregion
+}
