ae424421ddc610d4850c68e3912cb802103dd037
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-04-11 21:04:25 +0200

    Revert "Revert "Created a script to smoothly move and rotate a card""
    This reverts commit 3b846cc3e6c185a5e4b4cf2658c54d561aecf685.

---
+++ BussenOnline/Assets/Scripts/SingleUse/RaiseEventExample.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseEventExample : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
