af4e5dc8e7ae883313285d8c2fb61528d8cbbb96
Bram SIerhuis <35657766+BramSierhuis@users.noreply.github.com>
2020-04-12 01:12:52 +0200

    Moved the card move code into coroutines

--- BussenOnline/Assets/Scripts/SingleUse/CardFlip.cs
+++ BussenOnline/Assets/Scripts/SingleUse/CardFlip.cs
@@ -5,36 +5,36 @@ using UnityEngine;
 public class CardFlip : MonoBehaviour
 {
     public float speed = 5f;
-    public Transform to; //Y rotation has to be 0
+    public Transform toRef; //Y rotation has to be 0
     public Sprite cardFront; //The card has to be instantiated with its back on top
 
     private SpriteRenderer sr;
-    private float journeyLength;
-    private float time;
-    private float lerpValue;
-    private Transform from;
-    private bool frontShown = false;
 
     private void Awake()
     {
-        from = transform; //From y rotation has to be 180
-        from.eulerAngles = -transform.eulerAngles;
-
         sr = GetComponent<SpriteRenderer>();
     }
 
     void Start()
     {
-        journeyLength = Vector3.Distance(from.position, to.position);
+        StartCoroutine(MoveWithoutRotation(toRef));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MoveWithRotation(Transform to)
     {
-        if (transform.position != to.position)
+        float time = 0;
+        float lerpValue;
+        bool frontShown = false;
+        Transform from = transform; //From y rotation has to be 180
+
+        from.eulerAngles = -transform.eulerAngles;
+
+        float journeyLength = Vector3.Distance(from.position, to.position);
+
+        while (transform.position != to.position)
         {
             time += Time.deltaTime;
-            lerpValue = time / (1 / (speed/journeyLength));
+            lerpValue = time / (1 / (speed / journeyLength));
 
             if (transform.eulerAngles.y < 90 && !frontShown)
             {
@@ -44,6 +44,27 @@ public class CardFlip : MonoBehaviour
 
             transform.rotation = Quaternion.Lerp(from.rotation, to.rotation, Mathf.SmoothStep(0, 1, lerpValue));
             transform.position = Vector3.Lerp(from.position, to.position, Mathf.SmoothStep(0, 1, lerpValue));
+
+            yield return null;
+        }
+    }
+    
+    IEnumerator MoveWithoutRotation(Transform to)
+    {
+        float time = 0;
+        float lerpValue;
+        Transform from = transform; //From y rotation has to be 180
+
+        float journeyLength = Vector3.Distance(from.position, to.position);
+
+        while (transform.position != to.position)
+        {
+            time += Time.deltaTime;
+            lerpValue = time / (1 / (speed / journeyLength));
+
+            transform.position = Vector3.Lerp(from.position, to.position, Mathf.SmoothStep(0, 1, lerpValue));
+
+            yield return null;
         }
     }
 }
